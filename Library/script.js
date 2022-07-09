505490e06a96559f92b9d6a77548ae10a9e67675
Guruprasath Gopal <guruprasath3200@gmail.com>
2022-07-09 09:09:36 +0800

    Added Styling for New Card

--- Library/script.js
+++ Library/script.js
@@ -30,8 +30,13 @@ function inputBox(){
     var Delete = document.createElement('button')
     let div1= document.createElement('div');
     let div2= document.createElement('div');
-    let textarea1 = document.createElement('textarea')
-    let textarea2 = document.createElement('textarea')
+    let titleinput  = document.createElement('input')
+    let authorinput  = document.createElement('input')
+    let textarea1 = document.createElement('textarea');
+    let textarea2 = document.createElement('textarea');
+    let read_input =  document.createElement('input');
+    let total_input = document.createElement('input');
+    
     // Adding Classes
     maincontainer.classList.add('maincontainer')
     thecard.classList.add('thecard');
@@ -50,35 +55,48 @@ function inputBox(){
     Delete.classList.add('Delete');
     textarea1.classList.add('textarea1');
     textarea2.classList.add('textarea2');
+    read_input.classList.add("read_input")
+    total_input.classList.add("total_input")
+    titleinput.classList.add("title-input")
+    authorinput.classList.add("author-input")
+    
     // Adding TextContent To Button
     publish.textContent="Publish";
     publish.addEventListener('click',GetData)
     Delete.textContent= "Delete";
-    // Appending Stuff                                                                                                     
-    div1.appendChild(publish);
-    div2.appendChild(Delete)
-    let titleinput  = document.createElement('input')
+
+    // Adding Event Listeners & Setting up attributes                                                                                        
     titleinput.setAttribute('type', 'text');
-    title.appendChild(titleinput)
-    let authorinput  = document.createElement('input')
     authorinput.setAttribute('type', 'text')
-    author.appendChild(authorinput)
-    summary.appendChild(textarea1)
-    read.appendChild(document.createElement("input"))
-    total.appendChild(document.createElement("input"))
-    read.setAttribute('type','text');
-    total.setAttribute('type','text');
-    summary2.appendChild(textarea2)
+    read_input.setAttribute('type','number');
+    titleinput.required = "true";
+    authorinput.required = "true";
+    textarea1.required= "true";
+    read_input.required="true";
+    total_input.required="true";
+    textarea2.required="true";
+    total_input.setAttribute('type','number');
     icon.addEventListener('click',rotateCard)
     icon2.addEventListener('click',rotateCard)
+    Delete.addEventListener('click',deleteCard)
+
+    // Appending tags 
+    div1.appendChild(publish);
+    div2.appendChild(Delete)
+    title.appendChild(titleinput)
+    author.appendChild(authorinput)
+    summary.appendChild(textarea1)
+    summary2.appendChild(textarea2)
+    read.appendChild(read_input)
+    total.appendChild(total_input)
     pages.append(read,total);
     thefront.append(icon2,title,author,summary,icon,pages);
     theback.append(icon,summary2,div1,div2)
     thecard.append(thefront,theback);
     maincontainer.append(thecard);
-    document.querySelector('main').appendChild(maincontainer);
-}
 
+    document.querySelector('#lastcard').before(maincontainer)
+}
 
 let mylibrary = [];
 function Book(){
@@ -100,8 +118,9 @@ function GetData(e){
     let read  = (((e.path[3]).childNodes[0]).childNodes[4]).childNodes[0]
     let total  = (((e.path[3]).childNodes[0]).childNodes[4]).childNodes[0]
     let summary2  = ((e.path[3]).childNodes[1]).childNodes[1]
+}
 
-   
-
-
+function deleteCard(e){
+    e.path[4].remove() // Traversing to .maincontainer and remove card
+    console.log("Delete Card Function deleted a card")
 }
